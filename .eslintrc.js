06c2cf37ea33f711d39f2b3d08ff0ea610569a65
Alessandro Petrillo <aless.petrillo@gmail.com>
2020-06-09 22:55:45 -0400

    add sort config of imports to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,4 +13,45 @@ module.exports = {
     'react-native/react-native': true,
   },
   extends: ['@react-native-community', 'prettier', 'prettier/react'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'lib/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'res/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'navigation/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'screens/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'never',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
